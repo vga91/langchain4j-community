89647316bb4ce981c2318aa8b6889cdcce90ecd8
vga91 <giuseppe.villani@larus-ba.it>
2025-03-27 12:40:07 +0100

    Integration test for invalid query for Neo4jGraph

--- content-retrievers/langchain4j-community-neo4j-retriever/src/test/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4JText2CypherRetrieverTest.java
+++ content-retrievers/langchain4j-community-neo4j-retriever/src/test/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4JText2CypherRetrieverTest.java
@@ -145,4 +145,21 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
             assertThat(e.getMessage()).contains("Variable `author` not defined");
         }
     }
+
+    @Test
+    void shouldThrowsErrorWhenNeo4jGraphQueryIsInvalid() {
+        final String invalidQuery = "MATCH(movie:Movie {title: 'Dune'}) RETURN author.name AS output";
+
+        try {
+            graph.executeRead(invalidQuery);
+        } catch (RuntimeException e) {
+            assertThat(e.getMessage()).contains("Variable `author` not defined");
+        }
+
+        try {
+            graph.executeWrite(invalidQuery);
+        } catch (RuntimeException e) {
+            assertThat(e.getCause().getMessage()).contains("Variable `author` not defined");
+        }
+    }
 }
