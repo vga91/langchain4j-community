65db23f4b88f191f62bff98176e6278fbf86e7f2
vga91 <giuseppe.villani@larus-ba.it>
2025-03-28 11:49:54 +0100

    Issue 111: Add Neo4jText2CypherRetriever features

--- content-retrievers/langchain4j-community-neo4j-retriever/src/test/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4JText2CypherRetrieverTest.java
+++ content-retrievers/langchain4j-community-neo4j-retriever/src/test/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4JText2CypherRetrieverTest.java
@@ -2,10 +2,12 @@ package dev.langchain4j.rag.content.retriever.neo4j;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
+import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.rag.content.Content;
 import dev.langchain4j.rag.query.Query;
 import java.util.List;
@@ -37,8 +39,8 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
     void shouldRetrieveContentWhenQueryIsValid() {
         // Given
         Query query = new Query("Who is the author of the book 'Dune'?");
-        when(chatLanguageModel.chat(anyString()))
-                .thenReturn("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
 
         // When
         List<Content> contents = retriever.retrieve(query);
@@ -48,17 +50,123 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
     }
 
     @Test
+    void shouldReturnsEmptyIfMaxRetriesIsNegative() {
+        // Given
+        Query query = new Query("Who is the author of the book 'Dune'?");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH (n:Something) RETURN n"))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
+        
+        retriever = Neo4jText2CypherRetriever.builder()
+                .graph(graph)
+                .chatLanguageModel(chatLanguageModel)
+                .maxRetries(-1)
+                .build();
+
+        // When
+        List<Content> contentsWithCypherDSL = retriever.retrieve(query);
+
+        // Then
+        assertThat(contentsWithCypherDSL).isEmpty();
+    }
+
+    @Test
+    void shouldReturnsEmptyIfICallWithoutRetry() {
+        // Given
+        Query query = new Query("Who is the author of the book 'Dune'?");
+        
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH (n:Something) RETURN n"))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
+
+        retriever = Neo4jText2CypherRetriever.builder()
+                .graph(graph)
+                .chatLanguageModel(chatLanguageModel)
+                .maxRetries(1)
+                .build();
+
+        // When
+        List<Content> contentsWithCypherDSL = retriever.retrieve(query);
+
+        // Then
+        assertThat(contentsWithCypherDSL).isEmpty();
+    }
+
+    @Test
+    void shouldRetrieveResultIfICallWithRetryAndIfTheFirstTryIsEmpty() {
+        // Given
+        Query query = new Query("Who is the author of the book 'Dune'?");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH (n:Something) RETURN n"))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
+
+        retriever = Neo4jText2CypherRetriever.builder()
+                .graph(graph)
+                .chatLanguageModel(chatLanguageModel)
+                .build();
+
+        // When
+        List<Content> contentsWithCypherDSL = retriever.retrieve(query);
+
+        // Then
+        assertThat(contentsWithCypherDSL).hasSize(1);
+    }
+    
+
+    @Test
+    void shouldThrowsErrorIfICallWithoutRetry() {
+        // Given
+        Query query = new Query("Who is the author of the book 'Dune'?");
+        when(chatLanguageModel.chat(anyList()))
+                .thenThrow(new RuntimeException("Invalid statement"))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
+
+        retriever = Neo4jText2CypherRetriever.builder()
+                .graph(graph)
+                .chatLanguageModel(chatLanguageModel)
+                .maxRetries(1)
+                .build();
+                
+        try {
+            retriever.retrieve(query);
+            fail();
+        } catch (RuntimeException e) {
+            assertThat(e.getMessage()).contains("Invalid statement");
+        }
+    }
+
+    @Test
+    void shouldRetrieveResultIfICallWithRetryAndIfTheFirstTryHasError() {
+        // Given
+        Query query = new Query("Who is the author of the book 'Dune'?");
+        when(chatLanguageModel.chat(anyList()))
+                .thenThrow(new RuntimeException("Invalid statement"))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
+
+        retriever = Neo4jText2CypherRetriever.builder()
+                .graph(graph)
+                .chatLanguageModel(chatLanguageModel)
+                .build();
+
+        // When
+        List<Content> contentsWithCypherDSL = retriever.retrieve(query);
+
+        // Then
+        assertThat(contentsWithCypherDSL).hasSize(1);
+    }
+
+    @Test
     void shouldRetrieveContentWhenQueryIsValidWithCypherDSLFixing() {
         // Given
         Query query = new Query("Who is the author of the book 'Dune'?");
-        when(chatLanguageModel.chat(anyString()))
-                .thenReturn("MATCH(book:Book {title: 'Dune'})-[:WROTE]->(author:Person) RETURN author.name AS output");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})-[:WROTE]->(author:Person) RETURN author.name AS output"));
 
         // When
         List<Content> contents = retriever.retrieve(query);
 
         // Then
-        assertThat(contents).hasSize(0);
+        assertThat(contents).isEmpty();
 
         retriever = Neo4jText2CypherRetriever.builder()
                 .graph(graph)
@@ -77,8 +185,8 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
     void shouldRetrieveContentWhenQueryIsValidWithDeprecatedClass() {
         // Given
         Query query = new Query("Who is the author of the book 'Dune'?");
-        when(chatLanguageModel.chat(anyString()))
-                .thenReturn("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
 
         Neo4jContentRetriever retriever = Neo4jContentRetriever.builder()
                 .graph(graph)
@@ -96,9 +204,8 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
     void shouldRetrieveContentWhenQueryIsValidAndResponseHasBackticks() {
         // Given
         Query query = new Query("Who is the author of the book 'Dune'?");
-        when(chatLanguageModel.chat(anyString()))
-                .thenReturn(
-                        "```MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output```");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("```MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output```"));
 
         // When
         List<Content> contents = retriever.retrieve(query);
@@ -121,9 +228,8 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
     void shouldReturnEmptyListWhenQueryHasNoResults() {
         // Given
         Query query = new Query("Who is the author of the movie 'Dune'?");
-        when(chatLanguageModel.chat(anyString()))
-                .thenReturn(
-                        "MATCH(movie:Movie {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH(movie:Movie {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output"));
 
         // When
         List<Content> contents = retriever.retrieve(query);
@@ -136,8 +242,8 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
     void shouldThrowsErrorWhenCypherQueryIsInvalid() {
         // Given
         Query query = new Query("Who is the author of the movie 'Dune'?");
-        when(chatLanguageModel.chat(anyString()))
-                .thenReturn("MATCH(movie:Movie {title: 'Dune'}) RETURN author.name AS output");
+        when(chatLanguageModel.chat(anyList()))
+                .thenReturn(getChatResponse("MATCH(movie:Movie {title: 'Dune'}) RETURN author.name AS output"));
 
         try {
             retriever.retrieve(query);
@@ -161,5 +267,9 @@ public class Neo4JText2CypherRetrieverTest extends Neo4jText2CypherRetrieverBase
         } catch (RuntimeException e) {
             assertThat(e.getCause().getMessage()).contains("Variable `author` not defined");
         }
+    }
+
+    private static ChatResponse getChatResponse(String text) {
+        return ChatResponse.builder().aiMessage(AiMessage.from(text)).build();
     }
 }
